1d04559e22d24d5a5d6c440262fbd588c8e680b6
juliodelimas <iam@juliodelima.com.br>
2026-04-09 22:45:41 -0400

    Disable Cypress env exposure

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  allowCypressEnv: false,
   e2e: {
     baseUrl: "http://127.0.0.1:3000",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx}",
